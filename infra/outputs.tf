7ca94978bd93f7075db4b3dc64d9d77890bf0ee5
Eduarda Aguiar <73914795+eaguiara@users.noreply.github.com>
2026-07-13 23:13:35 -0300

    feat: rds

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,9 +1,9 @@
 output "cluster_name" {
   value = var.cluster_name
 }
-
 output "kubernetes_manifest_path" {
   value = var.k8s_manifest_path
+}
 output "sql_arn_id" {
     value = aws_db_instance.sql_server.arn
 }
